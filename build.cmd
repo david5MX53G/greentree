a2b8a479e28e2576929d8804120d6e7f0a52623d
David <david120@use.startmail.com>
2018-07-08 11:58:02 -0500

    updated set_ant.cmd and export_ant.sh to run Ant, renamed to build.sh and build.cmd, then updated README accordingly

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
-rem This sets up variables for Apache Ant
+rem This sets up variables for Apache Ant and runs the default target in build.xml using Ant.
+rem Ensure there is a valid Ant build.xml file in the same dir as this script before running.
 rem See "https://stackoverflow.com/questions/1851012/set-a-path-variable-with-spaces-in-the-path-in-a-windows-cmd-file-or-batch-file"
 rem See "Ant/Tutorial/AntTutorial.htm" Mike Prasad posted Jun 23, 2018 9:24 AM MSSE670_X40_Java Software Development
 set PATH=
@@ -6,3 +7,4 @@ set CLASSPATH=
 set "JAVA_HOME=C:\Program Files\Java\jdk1.8.0_171"
 set "ANT_HOME=C:\Program Files\Java\apache-ant-1.10.4"
 set PATH=%JAVA_HOME%\bin;.;%ANT_HOME%\bin
+ant
--- export_ant.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export JAVA_HOME=/opt/jdk1.8.0_171
-export ANT_HOME=/opt/apache-ant-1.10.4
-export PATH=$PATH:$JAVA_HOME/bin:$ANT_HOME/bin
