dee1dc054c27d5910611efb33d9474a8d12b0621
David <david120@use.startmail.com>
2018-07-14 16:58:09 -0500

    minor changes to LogJInternalFrame

--- src/com/greentree/view/viewlog/LogJInternalFrame.java
+++ src/com/greentree/view/viewlog/LogJInternalFrame.java
@@ -11,27 +11,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
 /**
- * This class displays the data of the <code>{@link Token#blockChain}</code> in a scrollable <code>
- * {@link JTextArea}</code> (as demonstrated in <a href=
+ * This class displays the data of the <code>{@link Token#blockChain}</code> in
+ * a scrollable {@link JTextArea} (as demonstrated in <a href=
  * "https://docs.oracle.com/javase/tutorial/uiswing/examples/components/TextDemoProject/src/components/TextDemo.java">
  * TextDemo.java</a>).
- * 
- * TODO: add a close button!!
- * 
+ *
  * @author david5MX53G
  *
  */
 public class LogJInternalFrame extends JInternalFrame {
-	/**
-	 * Eclipse has generated this <code>long</code> so the class can implement <code>{@link 
-	 * java.io.Serializable}</code>
-	 */
-	private static final long serialVersionUID = 6660852051586605385L;
 
-	
-	/** {@link Container} which holds all other elements in this {@link JFrame} */
-	Container cPane = getContentPane();
-    
+    /**
+     * Eclipse has generated this <code>long</code> so the class can implement <code>{@link
+     * java.io.Serializable}</code>
+     */
+    private static final long serialVersionUID = 6660852051586605385L;
+
+    /**
+     * {@link Container} which holds all other elements in this {@link JFrame}
+     */
+    Container cPane = getContentPane();
+
     /**
      * contains the log messages in the <code>{@link Token#blockChain}</code>
      */
@@ -41,51 +41,52 @@ public class LogJInternalFrame extends JInternalFrame {
      * provides scrolling for <code>{@link LogJInternalFrame#textArea}</code>
      */
     JScrollPane scrollPane = new JScrollPane(textArea);
-    
+
     /**
      * members for {@link GridBagLayout}
      */
     GridBagConstraints gBagC = new GridBagConstraints();
-	GridBagLayout gBagL = new GridBagLayout();
+    GridBagLayout gBagL = new GridBagLayout();
 
     /**
-     * Spawns a new {@link LogJInternalFrame} which renders all <code>String</code> objects in the 
-     * given <code>Iterator</code> in a JFrame.
-     * 
-     * @param it {@link Iterator}<{@link String}> displays its contents in the {@link 
+     * Spawns a new {@link LogJInternalFrame} which renders all
+     * <code>String</code> objects in the given <code>Iterator</code> in a
+     * JFrame.
+     *
+     * @param it {@link Iterator}<{@link String}> displays its contents in the {@link
      *     LogJInternalFrame#textArea}
      */
     public LogJInternalFrame(Iterator<String> it) {
-    	super("Log Messages",
-  	          true, //resizable
-  	          true, //closable
-  	          true, //maximizable
-  	          true);//iconifiable
-    	this.setDefaultCloseOperation(HIDE_ON_CLOSE);
-    	
-    	// set up container layout and constraints
-    	cPane.setLayout(gBagL);
-    	gBagC.ipadx = 250;
-		gBagC.gridwidth = GridBagConstraints.REMAINDER;
-		gBagC.fill = GridBagConstraints.BOTH;
-		gBagC.weightx = 1.0;
-		gBagC.weighty = 1.0;
-    	
-    	// add elements
-    	textArea.setEditable(false);
-    	cPane.add(scrollPane, gBagC);
-    	
-    	// draw frame
-    	this.pack();
-    	this.setVisible(true);
-        
+        super("Log Messages",
+            true, //resizable
+            true, //closable
+            true, //maximizable
+            true);//iconifiable
+        this.setDefaultCloseOperation(HIDE_ON_CLOSE);
+
+        // set up container layout and constraints
+        cPane.setLayout(gBagL);
+        gBagC.ipadx = 250;
+        gBagC.gridwidth = GridBagConstraints.REMAINDER;
+        gBagC.fill = GridBagConstraints.BOTH;
+        gBagC.weightx = 1.0;
+        gBagC.weighty = 1.0;
+
+        // add elements
+        textArea.setEditable(false);
+        cPane.add(scrollPane, gBagC);
+
+        // draw frame
+        this.pack();
+        this.setVisible(true);
+
         // fill with text
-    	if (it.hasNext()) {
-    		while (it.hasNext()) {
-				textArea.append(it.next() + "\n");
-			}
-    	} else {
-    		textArea.append("no messages");
-    	}
+        if (it.hasNext()) {
+            while (it.hasNext()) {
+                textArea.append(it.next() + "\n");
+            }
+        } else {
+            textArea.append("no messages");
+        }
     }
 }
