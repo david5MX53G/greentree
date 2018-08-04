47b3da56fcd782cdb17897fc7a7deaf3fc2da75a
David <david120@use.startmail.com>
2018-08-04 16:02:22 -0500

    minor cleanup in HibernateSessionFactory

--- src/com/greentree/model/services/factory/HibernateSessionFactory.java
+++ src/com/greentree/model/services/factory/HibernateSessionFactory.java
@@ -25,13 +25,10 @@ package com.greentree.model.services.factory;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 /**
  * This class is a {@link org.hibernate.SessionFactory} implementation that
