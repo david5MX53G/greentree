a2c4b2578716c4d378b28fc224c0681924e4fa9e
David <david120@use.startmail.com>
2018-08-04 15:41:20 -0500

    updated scripts

--- scripts/changes.sh
+++ scripts/changes.sh
@@ -1,1 +1,1 @@
-git log --since '2018-07-15' --pretty=format:'%cd "%s"' | sed -e 's/$/  /g' > CHANGES; python -m markdown CHANGES
+git log --since '2018-07-22' --pretty=format:'%cd "%s"' | sed -e 's/$/  /g' > CHANGES; python -m markdown CHANGES
--- scripts/zip.sh
+++
@@ -1,1 +0,0 @@
-cd ..;zip -r Dietrich_David_Week3.zip GreenTree/ -x *.git*
